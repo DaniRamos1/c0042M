fbdc6e4e2b275fe8a2bbd85fa36f01c5e3fde337
DaniRamos1 <135669453+DaniRamos1@users.noreply.github.com>
2023-06-06 00:45:59 +0200

    ejercicios

---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dramos-c <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/05 18:54:10 by dramos-c          #+#    #+#             */
+/*   Updated: 2023/06/06 00:36:49 by dramos-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	letra;
+
+	letra = 'z';
+	while (letra >= 'a')
+	{
+		write(1, &letra, 1);
+		letra--;
+	}
+}
+
+int	main(void)
+{
+	ft_print_reverse_alphabet(void);
+	return (0);
+}
